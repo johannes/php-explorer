639a2954e8af8dce5ff5b9a067f8debed9c28dca
Johannes Schlüter <johannes@schlueters.de>
2009-08-10 02:51:04 +0200

    Adopt search tool to new file layout

--- util/fulltextsearch.php
+++ util/fulltextsearch.php
@@ -5,13 +5,16 @@
  */
 
 error_reporting(E_ALL);
-include __DIR__.'/../manual.php';
+ini_set('include_path', __DIR__.'/../src'.PATH_SEPARATOR.ini_get('include_path'));
+require_once 'PHPUnit/Framework.php';
+
+require_once 'Explorer/Manual/Manual.php';
 
 if ($argc != 2) {
 	die("Usage: $argv[0] expression\n");
 }
 
-$manuals = new Explorer\Manual\Manual('.', 'en');
+$manuals = new Explorer\Manual\Manual( __DIR__.'/../data', 'en');
 $s = $manuals->searchFulltext($argv[1], true);
 foreach ($s as $title => $search) {
 	echo $title,"\n";
