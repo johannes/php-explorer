185e675e4bcfbc6fceb4fb2abb1009712dbd0b12
Johannes Schlüter <johannes@schlueters.de>
2009-08-12 22:45:33 +0200

    Some more checks

--- src/explorer.php
+++ src/explorer.php
@@ -2,17 +2,30 @@
 <?php
 error_reporting(E_ALL);
 
+function gtk_die($message) {
+    $dialog = new \GtkMessageDialog(null, 0, \Gtk::MESSAGE_ERROR, \Gtk::BUTTONS_OK, 'Extension missing');
+    $dialog->set_markup($message);
+    $dialog->run();
+    $dialog->destroy();
+    die($message."\n");
+}
+
 if (!extension_loaded('php-gtk')) {
     die("ERROR: PHP Gtk2 not loaded!\n");
 }
 
+if (version_compare('5.3.0', PHP_VERSION, '>')) {
+    gtk_die('PHP Explorer requires PHP 5.3.0 ornewer.');
+}
+
 if (!extension_loaded('phar')) {
-	$dialog = new \GtkMessageDialog(null, 0, \Gtk::MESSAGE_ERROR, \Gtk::BUTTONS_OK, 'Extension missing');
-	$message = 'PHAR Extension not loaded! Terminating.';
-	$dialog->set_markup($message);
-	$dialog->run();
-	$dialog->destroy();
-	die();
+    $message = 'PHAR Extension not loaded! Terminating.';
+    gtk_die($message);
+}
+
+if(!phar::canCompress()) {
+    $message = 'PHAR Extension has no compression support.';
+    gtk_die($message);
 }
 
 ini_set('include_path', 'phar://'.__FILE__.PATH_SEPARATOR.ini_get('include_path'));
