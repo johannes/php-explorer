aaafe197915d76c2e257524cae173a0431cdfe37
Johannes Schlüter <johannes@schlueters.de>
2009-08-14 02:04:56 +0200

    Add a summary and only report missing method docs for the declaring class

--- util/checkdoc.php
+++ util/checkdoc.php
@@ -9,6 +9,8 @@ ini_set('include_path', __DIR__.'/../src'.PATH_SEPARATOR.ini_get('include_path')
 
 require_once 'Explorer/Manual/Manual.php';
 
+$missing_ext = $missing_func = $missing_class = $missing_method = 0;
+
 $manuals = new Explorer\Manual\Manual( __DIR__.'/../data', 'en');
 
 foreach (get_loaded_extensions() as $ext) {
@@ -16,6 +18,7 @@ foreach (get_loaded_extensions() as $ext) {
 	$manuals->get(new ReflectionExtension($ext));
     } catch (Explorer\Manual\ManualPageNotFoundException $e) {
 	echo "missing extension: $ext\n";
+	$missing_ext++;
     }
 }
 
@@ -25,6 +28,7 @@ foreach ($funcs['internal'] as $func) {
 	$manuals->get(new ReflectionFunction($func));
     } catch (Explorer\Manual\ManualPageNotFoundException $e) {
 	echo "missing function: $func\n";
+	$missing_func++;
     }
 }
 
@@ -38,13 +42,19 @@ foreach (get_declared_classes() as $c) {
 	$manuals->get($rc);
     } catch (Explorer\Manual\ManualPageNotFoundException $e) {
 	echo "missing class: $c\n";
+	$missing_class++;
     }
 
     foreach ($rc->getMethods() as $name => $method) {
-	try {
-	    $manuals->get($method);
-        } catch (Explorer\Manual\ManualPageNotFoundException $e) {
-	    echo "missing method: $c::{$method->getName()}\n";
+        if ($method->getDeclaringClass() == $rc) {
+            try {
+                $manuals->get($method);
+            } catch (Explorer\Manual\ManualPageNotFoundException $e) {
+                echo "missing method: $c::{$method->getName()}\n";
+		$missing_method++;
+            }
 	}
     }
 }
+
+echo "Summary: Missing $missing_ext extensions, $missing_func funcs, $missing_class classes, $missing_method methods\n";
