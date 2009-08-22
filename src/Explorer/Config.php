7ae4ff52fecca6171366373a9b246be50290d3a1
Johannes Schlüter <johannes@schlueters.de>
2009-08-22 13:24:48 +0200

    Add option for saving the ini file

--- src/Explorer/Config.php
+++ src/Explorer/Config.php
@@ -2,6 +2,7 @@
 namespace Explorer;
 
 class Config implements \ArrayAccess {
+    private $changed = false;
     protected static $instance;
 
     protected $data = array();
@@ -16,7 +17,9 @@ class Config implements \ArrayAccess {
     /**
      * Returns a list of directories to search, can be overloaded for tests and such
      *
-     * The first directory in the array (index 0) has highest priority, the last the least
+     * The first directory in the array (index 0) has highest priority, the last the least,
+     * The first item should be the user's home so it can be written, the last
+     * should hold default values.
      *
      * @return array
      */
@@ -38,7 +41,7 @@ class Config implements \ArrayAccess {
             array_unshift($order, $overridefile);
         } else {
             // At index 0 I expect the user config for making it writable
-            $this->data[] = array();
+            //$this->data[] = array();
         }
 
         foreach ($order as $filename) {
@@ -54,12 +57,11 @@ class Config implements \ArrayAccess {
      * @return Explorer\config
      */
     static function getInstance($overridefile = null) {
-        if (self::$instance) {
-            return $instance;
-        } else {
+        if (!self::$instance) {
             $class = get_called_class();
-            return new $class($overridefile);
+            self::$instance = new $class($overridefile);
         }
+	return self::$instance;
     }
 
     public function getLoadedFiles() {
@@ -68,6 +70,48 @@ class Config implements \ArrayAccess {
             unset($data[0]);
         }
         return array_keys($data);
+    }
+
+    public function isDefault($offset) {
+        $local = reset($this->data);
+        $default = end($this->data);
+
+        if (!isset($local[$offset])) {
+	    return true;
+	}
+        if (!isset($default[$offset])) {
+            throw new ConfigValueNotFoundException($offset);
+        }
+
+        return $local[$offset] == $default[$offset];
+    }
+
+    public function save($location = null) {
+	if (!$this->changed) {
+	    return;
+	}
+        if (!$location) {
+	    $filelist = $this->getSearchList();
+            reset($filelist);
+            $location = current($filelist);
+        }
+
+        $fp = fopen($location, 'w');
+        fwrite($fp, "[PHP Explorer]\n");
+        $merged = array();
+        foreach ($this->data as $values) {
+            $merged = array_merge($merged, $values);
+        }
+        $keys = array_keys($merged);
+
+        foreach ($keys as $key) {
+            if ($this->isDefault($key)) {
+                continue;
+            }
+
+            fwrite($fp, $key.' = '.$this[$key]."\n");
+        }
+        fclose($fp);
     }
 
     public function offsetGet($offset) {
@@ -91,11 +135,13 @@ class Config implements \ArrayAccess {
     }
 
     public function offsetSet($offset, $value) {
+        $this->changed = true;
         $this->data[0][$offset] = $value;
     }
 
     
     public function offsetUnset($offset) {
+        $this->changed = true;
         unset($this->data[0][$offset]);
     }
 }
